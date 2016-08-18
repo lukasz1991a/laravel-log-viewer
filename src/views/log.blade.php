fb71edc13458a99aedeb2aced692d46109060825
lukasz1991a <lukasz.kucza@optimalit.pl>
2016-08-18 09:21:59 +0200

    Update log.blade.php

--- src/views/log.blade.php
+++ src/views/log.blade.php
@@ -1,53 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Laravel log viewer</title>
+@extends('admin.layouts.main')
 
-    <!-- Bootstrap -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdn.datatables.net/plug-ins/9dcbecd42ad/integration/bootstrap/3/dataTables.bootstrap.css">
-
-
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-    <style>
-      body {
-        padding: 25px;
-      }
-      h1 {
-        font-size: 1.5em;
-        margin-top: 0px;
-      }
-      .stack {
-        font-size: 0.85em;
-      }
-      .date {
-        min-width: 75px;
-      }
-      .text {
-        word-break: break-all;
-      }
-      a.llv-active {
-        z-index: 2;
-        background-color: #f5f5f5;
-        border-color: #777;
-      }
-    </style>
-  </head>
-  <body>
+<!-- Main Content -->
+@section('content')
+<h2 class="page-title users-title">
+    Logi
+</h2>
     <div class="container-fluid">
       <div class="row">
         <div class="col-sm-3 col-md-2 sidebar">
-          <h1><span class="glyphicon glyphicon-calendar" aria-hidden="true"></span> Laravel Log Viewer</h1>
-          <p class="text-muted"><i>by Rap2h</i></p>
           <div class="list-group">
             @foreach($files as $file)
               <a href="?l={{ base64_encode($file) }}" class="list-group-item @if ($current_file == $file) llv-active @endif">
@@ -66,7 +26,6 @@
             <thead>
               <tr>
                 <th>Level</th>
-                <th>Context</th>
                 <th>Date</th>
                 <th>Content</th>
               </tr>
@@ -76,7 +35,6 @@
 @foreach($logs as $key => $log)
 <tr>
   <td class="text-{{{$log['level_class']}}}"><span class="glyphicon glyphicon-{{{$log['level_img']}}}-sign" aria-hidden="true"></span> &nbsp;{{$log['level']}}</td>
-  <td class="text">{{$log['context']}}</td>
   <td class="date">{{{$log['date']}}}</td>
   <td class="text">
     @if ($log['stack']) <a class="pull-right expand btn btn-default btn-xs" data-display="stack{{{$key}}}"><span class="glyphicon glyphicon-search"></span></a>@endif
@@ -92,20 +50,18 @@
           @endif
           <div>
             <a href="?dl={{ base64_encode($current_file) }}"><span class="glyphicon glyphicon-download-alt"></span> Download file</a>
-            -
-            <a id="delete-log" href="?del={{ base64_encode($current_file) }}"><span class="glyphicon glyphicon-trash"></span> Delete file</a>
+            {{-- -
+            <a id="delete-log" href="?del={{ base64_encode($current_file) }}"><span class="glyphicon glyphicon-trash"></span> Delete file</a> --}}
           </div>
         </div>
       </div>
     </div>
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script src="https://cdn.datatables.net/1.10.4/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/plug-ins/9dcbecd42ad/integration/bootstrap/3/dataTables.bootstrap.js"></script>
     <script>
       $(document).ready(function(){
         $('#table-log').DataTable({
           "order": [ 1, 'desc' ],
+          "iDisplayLength": getCookie("iDisplayLength"),
+          "lengthMenu": [[10, 25, 50, 100, 200], [10, 25, 50, 100, 200]],
           "stateSave": true,
           "stateSaveCallback": function (settings, data) {
             window.localStorage.setItem("datatable", JSON.stringify(data));
@@ -116,6 +72,10 @@
             return data;
           }
         });
+
+        $('.table-container').on( 'length.dt', function ( e, settings, len ) {
+          setCookie("iDisplayLength", len, 300);
+        });
         $('.table-container').on('click', '.expand', function(){
           $('#' + $(this).data('display')).toggle();
         });
@@ -124,5 +84,7 @@
         });
       });
     </script>
-  </body>
-</html>
+@endsection
+
+@section('head')
+@endsection
